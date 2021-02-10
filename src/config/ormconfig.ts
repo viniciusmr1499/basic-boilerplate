ac436d7fe22856ca4a28ffeeb1e33c4e308ad682
Marcos  Oliveira <marcos.oliveira@fortbrasil.com.br>
2021-02-10 14:47:52 -0300

    - Estrutura inicial da aplicação - Configuração do Docker e Docker Compose

--- src/config/ormconfig.ts
+++ src/config/ormconfig.ts
@@ -19,23 +19,23 @@ export default () =>
     },
 
     // Conexão com a Stage
-    {
-      name: 'stage',
-      type: 'mssql',
-      host: process.env.DB_STAGE_HOST,
-      port: Number(process.env.DB_PORT || 1433),
-      username: process.env.DB_STAGE_USER,
-      password: process.env.DB_STAGE_PASS,
-      database: process.env.DB_STAGE_NAME,
-      logging: process.env.NODE_ENV !== 'production' ? 'all' : ['error'],
-      entities: [`${__dirname}/../entities/stage/*{.ts,.js}`],
-      cli: {
-        entitiesDir: `${__dirname}/../entities/stage`,
-      },
-      extra: {
-        options: {
-          enableArithAbort: true,
-        },
-      },
-    },
+    // {
+    //   name: 'stage',
+    //   type: 'mssql',
+    //   host: process.env.DB_STAGE_HOST,
+    //   port: Number(process.env.DB_PORT || 1433),
+    //   username: process.env.DB_STAGE_USER,
+    //   password: process.env.DB_STAGE_PASS,
+    //   database: process.env.DB_STAGE_NAME,
+    //   logging: process.env.NODE_ENV !== 'production' ? 'all' : ['error'],
+    //   entities: [`${__dirname}/../entities/stage/*{.ts,.js}`],
+    //   cli: {
+    //     entitiesDir: `${__dirname}/../entities/stage`,
+    //   },
+    //   extra: {
+    //     options: {
+    //       enableArithAbort: true,
+    //     },
+    //   },
+    // },
   ];
